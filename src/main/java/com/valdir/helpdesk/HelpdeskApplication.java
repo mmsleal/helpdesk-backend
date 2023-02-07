3f56f80458f8badb1c31bb0ed953fc005c0ee8b3
mmsleal <mmsleal@gmail.com>
2023-02-06 23:20:04 -0300

    carga inicial da base de dados

--- src/main/java/com/valdir/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/valdir/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,57 @@
 package com.valdir.helpdesk;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HelpdeskApplication {
+import com.valdir.helpdesk.domain.Chamado;
+import com.valdir.helpdesk.domain.Cliente;
+import com.valdir.helpdesk.domain.Tecnico;
+import com.valdir.helpdesk.domain.enuns.Perfil;
+import com.valdir.helpdesk.domain.enuns.Prioridade;
+import com.valdir.helpdesk.domain.enuns.Status;
+import com.valdir.helpdesk.repositories.ChamadoRepository;
+import com.valdir.helpdesk.repositories.ClienteRepository;
+import com.valdir.helpdesk.repositories.TecnicoRepository;
 
+
+@SpringBootApplication
+public class HelpdeskApplication implements CommandLineRunner {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Tecnico tec1 = new Tecnico(null, "Valdir Cezar", "06605469577", "valdir@mail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		
+		
+		Cliente cli1 = new Cliente(null, "Linus Torvals", "95750048926", "torvals@mail.com", "123");
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
+		
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+		
+		
 	}
 
 }
