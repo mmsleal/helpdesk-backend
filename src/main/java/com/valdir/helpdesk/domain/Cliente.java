6a142ad12a48cbf9aeaf9779bccc42fb2d6f3a39
mmsleal <mmsleal@gmail.com>
2023-02-08 08:42:29 -0300

    CRUD para cliente implementado

--- src/main/java/com/valdir/helpdesk/domain/Cliente.java
+++ src/main/java/com/valdir/helpdesk/domain/Cliente.java
@@ -2,8 +2,10 @@ package com.valdir.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.valdir.helpdesk.domain.dtos.ClienteDTO;
 import com.valdir.helpdesk.domain.enuns.Perfil;
 
 import jakarta.persistence.Entity;
@@ -25,6 +27,17 @@ public class Cliente extends Pessoa {
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x->x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
