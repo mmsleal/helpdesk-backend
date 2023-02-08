a0d003f73dc42fdbbf2c5dadc301922e32e6a5e3
mmsleal <mmsleal@gmail.com>
2023-02-07 21:54:43 -0300

    endpoint findAll para tecnico

--- src/main/java/com/valdir/helpdesk/services/TecnicoService.java
+++ src/main/java/com/valdir/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.valdir.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,10 @@ public class TecnicoService {
 		Optional<Tecnico> obj = repository.findById(id);
 		
 		return obj.orElseThrow(()->new ObjectNotFoundException("Objeto não encontrado! Id: "+ id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
 	}
 	
 }
